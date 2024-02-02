261e31f544f98577ff1c58710292cb4ccfdaa2a1
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-02 13:29:21 +0530

    create the statboard component

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,47 @@
+import briefcase from "../Assets/Briefcase.png";
+import statBoard from "../Assets/StatBoard.png";
+import circleMenu from "../Assets/Circled Menu.png";
+import support from "../Assets/Support.png";
+import plugins from "../Assets/Puzzle.png";
+import help from "../Assets/Help.png";
+import ShutDown from "../Assets/Shutdown.png";
+
 function Dashboard() {
   return (
     <div className="flex">
       {/* left and right side */}
-      <div className="w-60 h-screen bg-slate-700"></div>
+      <div className="w-60 h-screen bg-slate-700 flex flex-col justify-between">
+        {/* briefcase */}
+        <div className="flex flex-col justify-center items-center">
+          <img className="self-center" src={briefcase} alt="briefcase" />
+          <img src={statBoard} />
+        </div>
+
+        {/* pages */}
+        <div className="flex flex-col gap-8 mx-auto w-full">
+          <div className="ml-4 flex gap-3 justify-start items-center bg-gray-800 p-3 rounded-md">
+            <img src={circleMenu} alt="circlemenu" />
+            <p className="text-white">Dashboard</p>
+          </div>
+          <div className="ml-4 flex gap-3 justify-start items-center bg-gray-800 p-3 rounded-md">
+            <img src={support} alt="support" />
+            <p className="text-white">Support</p>
+          </div>
+          <div className="ml-4 flex gap-3 justify-start items-center bg-gray-800 p-3 rounded-md">
+            <img src={plugins} alt="plugin" />
+            <p className="text-white">Plugins</p>
+          </div>
+          <div className="ml-4 flex gap-3 justify-start items-center bg-gray-800 p-3 rounded-md">
+            <img src={help} alt="help" />
+            <p className="text-white">Help</p>
+          </div>
+        </div>
+        {/* button */}
+        <div className="flex justify-center items-center gap-2 text-red-500 w-full bg-white p-3">
+          <button>Logout</button>
+          <img src={ShutDown} alt="shutdown" />
+        </div>
+      </div>
       <div className="h-screen w-full bg-slate-200">
         {/* top section */}
         <div className="flex justify-between max-w-6xl mx-auto p-2">
@@ -19,6 +58,10 @@ function Dashboard() {
             />
           </div>
         </div>
+        {/* end of top section */}
+        {/* mid section */}
+        <div></div>
+        {/*end of mid section */}
       </div>
       {/* left and right side */}
     </div>
